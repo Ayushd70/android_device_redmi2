a81e2982a622b8ab7135d6749e5f3e99eee53cc7
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-11-12 15:48:17 -0700

    wt88047: Set TARGET_BOOTANIMATION_HALF_RES to true
    * No apparent resolution difference
    * Much smoother animation
    * Much faster boot
    * What's not to like?
    
    Change-Id: I2119fe826146a3103da50b4862ad88f5950a97ec

--- device.mk
+++ device.mk
@@ -342,6 +342,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
+TARGET_BOOTANIMATION_HALF_RES := true
 
 # Screen density
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -416,6 +417,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/vendor/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/vendor/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/vendor/etc/mixer_paths_qrd_skui.xml
+
 
 # Permissions
 PRODUCT_COPY_FILES += \

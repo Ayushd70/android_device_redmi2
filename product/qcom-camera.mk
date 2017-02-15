4958aff1b5962d890510ea5dff5ed42258a8eb63
NeoDarkness <aldi.45ckr@gmail.com>
2017-02-15 15:16:06 +0700

    wt88047: Reorder properties and flags

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -15,7 +15,7 @@ PRODUCT_COPY_FILES += \
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.cpp.duplication=false \
-    persist.camera.hal.debug.mask=0 \
     media.stagefright.legacyencoder=true \
-    media.stagefright.less-secure=true
+    media.stagefright.less-secure=true \
+    persist.camera.cpp.duplication=false \
+    persist.camera.hal.debug.mask=0
--- product/usb.mk
+++ product/usb.mk
@@ -1,6 +1,5 @@
 # USB ID
 ADDITIONAL_DEFAULT_PROPERTIES += \
-    ro.usb.vid=2717 \
     ro.usb.id.midi=90BA \
     ro.usb.id.midi_adb=90BB \
     ro.usb.id.mtp=ff60 \
@@ -8,7 +7,8 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.usb.id.ptp=ff10 \
     ro.usb.id.ptp_adb=ff18 \
     ro.usb.id.ums=ff20 \
-    ro.usb.id.ums_adb=ff28
+    ro.usb.id.ums_adb=ff28 \
+    ro.usb.vid=2717 
 
 # Permissions
 PRODUCT_COPY_FILES += \

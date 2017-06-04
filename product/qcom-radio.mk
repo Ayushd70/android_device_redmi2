8c6c6709265f2c7a67f80874ae90410e9058be0d
NeoDarkness <aldi.45ckr@gmail.com>
2017-06-04 12:37:45 +0700

    wt88047: Remove ril subscription types prop

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -5,5 +5,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.no_cons_man_roam=1 \
     ril.ecclist=000,08,100,101,102,110,112,118,119,120,122,911,999 \
     ril.ecclist1=000,08,100,101,102,110,112,118,119,120,122,911,999 \
-    rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so \
-    ril.subscription.types=RUIM
+    rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so

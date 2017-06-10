0bd2218dac340e70846f84a8940e381fff95f3b6
NeoDarkness <aldi.45ckr@gmail.com>
2017-06-10 13:45:20 +0700

    wt88047: Cleanup Camera

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -73,7 +73,7 @@ LOCAL_C_INCLUDES += \
         $(call project-path-for,qcom-display)/libqservice
 
 #ifeq ($(TARGET_USE_VENDOR_CAMERA_EXT),true)
-#LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/msm8974/libgralloc
+#LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
 #else
 LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
 #endif

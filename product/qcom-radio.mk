7c357b3b50174f52202f6803b1864af08274e702
NeoDarkness <aldi.45ckr@gmail.com>
2017-06-15 14:33:24 +0700

    wt88047: Ceanup device tree
    Change-Id: I258cfed10231bea767546c90b226247c38d99d65

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -2,7 +2,6 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.target=dpm1 \
     persist.radio.multisim.config=dsds \
-    persist.radio.no_cons_man_roam=1 \
     ril.ecclist=000,08,100,101,102,110,112,118,119,120,122,911,999 \
     ril.ecclist1=000,08,100,101,102,110,112,118,119,120,122,911,999 \
     rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so

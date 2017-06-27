a06c16cb9eb321851d1a303eb7d5fd7c3b4dbabe
Michael Bestas <mikeioannina@cyanogenmod.org>
2017-06-27 20:29:05 +0700

    wt88047: Allow vendor init to overide props

--- init/init_wt88047.cpp
+++ init/init_wt88047.cpp
@@ -33,6 +33,8 @@
 #include <fstream>
 #include <string>
 #include <sys/sysinfo.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
@@ -65,6 +67,17 @@ void check_device()
         heapsize = "256m";
         heapminfree = "2m";
     }
+}
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
 void init_target_properties()

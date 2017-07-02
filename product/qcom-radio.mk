289b4f0dc3eae3ab3bb18673b8e6ade4016e8c2b
NeoDarkness <aldi.45ckr@gmail.com>
2017-07-02 20:55:29 +0700

    Revert "Revert "wt88047: Move data configuration file to device tree [1/2]""
    This reverts commit a2e004282a02b3d949cb3333d7dfcb1273ef3b9b.

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -6,3 +6,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ril.ecclist1=000,08,100,101,102,110,112,118,119,120,122,911,999 \
     rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so \
     ro.telephony.default_network=9,9
+
+# RIL
+ifeq ($(QCPATH),)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
+    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml
+endif

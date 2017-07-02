919eb9a45da554ce2d7847923fbd8fd7e1d9c969
NeoDarkness <aldi.45ckr@gmail.com>
2017-07-02 20:45:28 +0700

    wt88047: Remove unused sensor properties

--- product/sensors.mk
+++ product/sensors.mk
@@ -12,7 +12,3 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
-
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.fusion.magfield.max=250

12f103ca5c1f354d48dd6427968bbfcac858cdd4
Isaac Chen <ycchen@google.com>
2017-10-24 15:09:36 +0800

    wt88047: Set TARGET_2ND_ARCH_VARIANT to armv8-a
    CYNGN-8916 targets are armv8 devices. Setting TARGET_2ND_ARCH_VARIANT
    to armv8-a, instead of armv7-a-neon, can silence the related warning
    during build.
    
    Bug: 68012286
    Change-Id: I18ae3aa985fd6bff11f043a155f3685d9f6644b8
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,7 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 else
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53

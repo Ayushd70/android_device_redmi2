d27fa2dc4db7b79e1a3536f3dfea6404d8b25c16
karthikkm007 <jacksparrow941994@gmail.com>
2018-03-31 17:47:57 +0530

    wt88047:This might fix audio and headphone

--- product/media.mk
+++ product/media.mk
@@ -1,4 +1,8 @@
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
-    $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/configs/mixer_paths_qrd_skui.xml:system/vendor/etc/mixer_paths_qrd_skui.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.media.treble_omx=false

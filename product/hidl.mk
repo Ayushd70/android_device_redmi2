5f480586f9235381aeb9a9600a678b133ef1b4d1
karthikkm007 <jacksparrow941994@gmail.com>
2018-03-31 17:51:13 +0530

    wt88047:to fix audio and headset this file might be required

--- product/hidl.mk
+++ product/hidl.mk
@@ -1,6 +1,7 @@
 # HIDL
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml 
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml \
+    $(LOCAL_PATH)/compatibility_matrix.xml:system/vendor/compatibility_matrix.xml
 
 # Temp disable media treble until i fix OMX decoder for inapp players
 PRODUCT_PROPERTY_OVERRIDES += \

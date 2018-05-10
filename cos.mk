ac52172d01af44832d1f28f9c1edfee1d0be1efa
Ayush Dubey <ayushdubey70@gmail.com>
2018-05-10 07:46:15 +0000

    CosmicOS bringup wt88047: Set build fingerprint instead of overriding it
    * This lets it be picked for both `ro.build.fingerprint` and
      `ro.vendor.build.fingerprint`, thus avoiding "vendor mismatch" issues.wt88047: Set build fingerprint instead of overriding it
    
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- cos.mk
+++ cos.mk
@@ -16,7 +16,7 @@
 $(call inherit-product, device/wingtech/wt88047/full_wt88047.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cos/common.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
@@ -24,7 +24,7 @@ TARGET_BOARD_PLATFORM_VARIANT := msm8916
 # Assert
 TARGET_OTA_ASSERT_DEVICE := HM2014811,HM2014812,HM2014813,HM2014814,HM2014815,HM2014816,HM2014817,HM2014818,HM2014819,HM2014820,HM2014821,HM2014112,wt88047,wt86047
 
-PRODUCT_NAME := lineage_wt88047
+PRODUCT_NAME := cos_wt88047
 BOARD_VENDOR := wingtech
 PRODUCT_DEVICE := wt88047
 
@@ -32,5 +32,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Build fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="Xiaomi/wt88047/wt88047:5.1.1/LMY47V/V8.1.3.0.LHJMIDL:user/release-keys" \
+    
     PRIVATE_BUILD_DESC="wt88047-user 5.1.1 LMY47V V8.1.3.0.LHJMIDL release-keys"
+
+ BUILD_FINGERPRINT=Xiaomi/wt88047/wt88047:5.1.1/LMY47V/V9.2.5.0.LHJMIEK:user/release-key

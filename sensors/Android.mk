jpc0/android_device_wingtech_wt88047

wt88047: Build our MultiHAL sensors HAL
Change-Id: I32172a0a6f5b3bf15ead878e91c2edd25b6f1625

sensors/Android.mk
@@ -27,8 +27,7 @@ LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libutils
 
-LOCAL_MODULE := sensors.msm8916
-LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := sensors.$(TARGER_DEVICE)
 LOCAL_MODULE_TAGS := optional
 
 # Export calibration library needed dependency headers

Ayushd70/android_device_redmi2

sensors/Android.mk
@@ -27,8 +27,7 @@ LOCAL_ADDITIONAL_DEPENDENCIES := INSTALLED_KERNEL_HEADERS
 
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libutils
 
-LOCAL_MODULE := sensors.msm8916
-LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := sensors.$(TARGET_DEVICE)
 LOCAL_MODULE_TAGS := optional
 
 # Export calibration library needed dependency headers
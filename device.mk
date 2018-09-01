24fe40c9df555da67d117594cdad0a1d71aea9bc
Daniel Jarai <jaraidaniel@gmail.com>
2018-09-02 00:34:14 +0200

    wt88047: Make the external camera provider ignore internal cameras
    * The external camera provider occupies our camera v4l2 nodes,
      potentially crashing the kernel driver and blocking the camera HAL.
      Unfortunately, there's no easy way to just disable it, so let's just
      tell it to ignore the internal video devices.
    
    Change-Id: I00c67de1b94a1f7eca1bf458d1d0e4de3507aa81
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- device.mk
+++ device.mk
@@ -95,10 +95,15 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service
 
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+
 
 # Display
 PRODUCT_PACKAGES += \

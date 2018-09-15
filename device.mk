7b5f1961478fd730167773fd98a7e944561e758f
Ayush Dubey <ayushdubey70@gmail.com>
2018-09-15 10:14:01 +0000

    wt88047:don't build FM
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- device.mk
+++ device.mk
@@ -112,10 +112,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service
 
-# FM
-PRODUCT_PACKAGES += \
-    FMRadio \
-    libfmjni
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \

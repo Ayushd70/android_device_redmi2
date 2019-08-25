be7b51fdb5495afd21d8ce8528ca89eb49b29261
Ayush Dubey <ayushdubey70@gmail.com>
2019-08-25 17:07:53 +0530

    wt88047:Add telephony-ext to bootjars
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- device.mk
+++ device.mk
@@ -338,6 +338,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/_hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/_hals.conf
 
+# Telephony
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
+
 # Thermal
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
